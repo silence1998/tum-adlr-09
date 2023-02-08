3c74b232787f099c1475d07db9712a89e876794e
volkan <volkantatlikazan@gmail.com>
2023-02-08 04:55:25 +0100

    for test w/o action smoothing

--- SAC-X/accuracy_calculation.py
+++ SAC-X/accuracy_calculation.py
@@ -1,12 +1,12 @@
 import torch
 import numpy as np
 from itertools import count
+import pickle
 
 from environment import GridWorldEnv
-from training import init_model, select_action
+from training import init_model, select_action, obstacle_sort, select_action_smooth, Scheduler
 
 from model import *
-from training import *
 import json
 
 if __name__ == '__main__':
@@ -43,6 +43,9 @@ if __name__ == '__main__':
         model_path = "test_models/Sa-t7/"
     elif m == "9":
         model_path = "test_models/Sa-t9/"
+    elif m == "3000":
+        model_path = "test_models/3000normaltrain_allfeatures/"
+
 
     # Load the model parameters
     with open(model_path + 'env_parameters.txt', 'r') as file:
@@ -53,63 +56,78 @@ if __name__ == '__main__':
         reward_parameters = json.load(file)
     with open(model_path + 'feature_parameters.txt', 'r') as file:
         feature_parameters = json.load(file)
+    tasks = (0, 1, 2)
+    sac_schedule = Scheduler(tasks)
 
-
-
+    with open(model_path + "Q_task.pkl", "rb") as tf:
+        sac_schedule.Q_task.store = pickle.load(tf)
     # initialize environment
-    env = GridWorldEnv(render_mode=None, object_size=env_parameters['object_size'], num_obstacles=env_parameters['num_obstacles'])
-    # env.render_mode = "human"
+    env = GridWorldEnv(render_mode=None,
+                       object_size=env_parameters['object_size'],  # TODO: change back to env_size to radius objects
+                       num_obstacles=env_parameters['num_obstacles'],
+                       window_size=env_parameters['window_size'])
 
     # initialize NN
-    actorNet, criticNet_1, criticNet_2, valueNet, target_valueNet, memory = init_model()
-    seed = feature_parameters['seed_init_value']
+    actorNet, criticNet_1, criticNet_2, valueNet, target_valueNet, memory = init_model(hyper_parameters["input_dims"])
     # Load model
     actorNet.load_state_dict(torch.load(model_path + "actor.pt", map_location=device))
-    actorNet.max_sigma = hyper_parameters['sigma_final']
+    # actorNet.max_sigma = hyper_parameters['sigma_final']
     criticNet_1.load_state_dict(torch.load(model_path + "criticNet_1.pt", map_location=device))
     criticNet_2.load_state_dict(torch.load(model_path + "criticNet_2.pt", map_location=device))
     target_valueNet.load_state_dict(torch.load(model_path + "target_valueNet.pt", map_location=device))
+    # actorNet.max_sigma = 0.1
+    # env=GridWorldEnv(render_mode="human")
 
-    init_seed = 0  # unseen envs
-    #seed = feature_parameters['seed_init_value']  # for seen envs
+
+    seed = feature_parameters['seed_init_value']  # training set
+    init_seed = 0
     actual_reward = []
     issuccess_ = []
     actual_step = []
     i = 0
-    seed = init_seed
-    while i < 100:  # run plot for 100 episodes to see what it learned
+    task = 0
+    seed = init_seed  # TODO: unseen test set, comment out for envs from training set
+    while i < 200:  # run plot for 10 episodes to see what it learned
+        List_Tau = []
         i += 1
-        seed += 1
+        print("Test run: ", str(i))
+        action_history = deque(maxlen=feature_parameters['action_history_size'])
+        # Initialize the environment and state
         env.reset(seed=seed)
+        seed += 1
         obs = env._get_obs()
-
+        if feature_parameters['sort_obstacles']:
+            obs = obstacle_sort(obs)
         obs_values = np.append(obs["agent"], obs["target"])
         for idx_obstacle in range(env_parameters['num_obstacles']):
-            np.append(obs_values, obs["obstacle_{0}".format(idx_obstacle)])
+            obs_values = np.append(obs_values, obs["obstacle_{0}".format(idx_obstacle)])
         state = torch.tensor(np.array(obs_values), dtype=torch.float, device=device)
 
         state = state.view(1, -1)
-        for t in count():
+        for t in count():  # every step of the environment
             # Select and perform an action
+            if t % sac_schedule.xi == 0:
+                fuzzy_state = sac_schedule.caluculate_fuzzy_distance(obs_values)
+                # task = random.choice(tasks) ## sac-u
+                task = sac_schedule.schedule_task(List_Tau, fuzzy_state)  ## sac-q
+                # print(task)
+                List_Tau.append(task)
+
             action = select_action(state, actorNet, task)
+            if feature_parameters['action_smoothing']:
+                action_history.extend([action])
+                action = select_action_smooth(action_history)
             _, reward, done, _, _ = env.step(action)
+            reward = torch.tensor([reward[task]], dtype=torch.float, device=device)
 
-            action_ = torch.tensor(action, dtype=torch.float, device=device)
-            action_ = action_.view(1, 2)
-            mu, sigma = actorNet(state)
-            # print(actorNet(state))
-            # print(criticNet_1(state, action_))
-            # print(criticNet_2(state, action_))
-            # print(target_valueNet(state))
-
-            reward = torch.tensor([reward], device=device)
-            env._render_frame()
             # Observe new state
             obs = env._get_obs()
             if not done:
+                if feature_parameters['sort_obstacles']:
+                    obs = obstacle_sort(obs)
                 obs_values = np.append(obs["agent"], obs["target"])
                 for idx_obstacle in range(env_parameters['num_obstacles']):
-                    np.append(obs_values, obs["obstacle_{0}".format(idx_obstacle)])
+                    obs_values = np.append(obs_values, obs["obstacle_{0}".format(idx_obstacle)])
                 next_state = torch.tensor(np.array(obs_values), dtype=torch.float, device=device)
 
                 next_state = next_state.view(1, -1)
@@ -117,6 +135,8 @@ if __name__ == '__main__':
                 next_state = None
 
             # Store the transition in memory
+            action = np.array([action])
+            action = torch.tensor(action, dtype=torch.float).to(actorNet.device)
             memory.push(state, action, next_state, reward)
 
             # Move to the next state
@@ -130,6 +150,7 @@ if __name__ == '__main__':
                 else:
                     issuccess_.append(0)
                 break
+
             elif t >= 500:
                 issuccess_.append(0)
                 actual_reward.append(0)
@@ -140,6 +161,9 @@ if __name__ == '__main__':
     # print(actual_reward)
     print("accuracy=", np.sum(issuccess_) / len(issuccess_))
     print("mean_reward=", np.mean(actual_reward))
-    print(actual_reward)
-    print("mean_step=", np.mean(actual_step))
-    print(actual_step)
+
+    print("std_reward=", np.std(actual_reward))
+
+    print("mean_step=", np.mean(actual_step))  # mean step duration
+
+    print("std_step=", np.std(actual_step))
